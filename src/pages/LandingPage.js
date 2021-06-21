7d6153be2c0b9642d802be93a5d6c3a1589f42ad
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-21 22:41:21 +0100

    update: add landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,14 +3,15 @@ import tw from "twin.macro";
 import { getJobs } from "services/api.service";
 
 const Container = tw.div`w-full text-gray-800`;
-const Header = tw.header`py-24 md:py-48 text-center  grid place-items-center h-screen`;
-const HeaderContent = tw.div`p-8`;
+const Header = tw.header`pt-24 md:py-32 text-center md:h-screen md:relative`;
+const HeaderContent = tw.div`grid place-items-center p-8`;
+const JobsNav = tw.div`lg:absolute bottom-20 inset-x-12 flex flex-col md:flex-row justify-between`;
 const Heading = tw.h1`text-green-500 text-6xl md:text-8xl font-bold`;
 const Description = tw.p`text-lg my-8 tracking-wide text-gray-700`;
 const ButtonRow = tw.div`mt-4 flex flex-col md:flex-row md:justify-between`;
 const Button = tw.button`px-24 py-3 mx-4 rounded-lg font-bold text-white mt-5 hocus:bg-green-600`;
-const SignUpButton = tw(Button)`bg-green-500`;
-const LogInButton = tw(Button)`border border-green-500 text-green-500 hocus:text-white`;
+const ButtonPrimary = tw(Button)`bg-green-500`;
+const ButtonOutline = tw(Button)`border border-green-500 text-green-500 hocus:text-white`;
 
 const LandingPage = () => {
 
@@ -76,44 +77,67 @@ const LandingPage = () => {
                     <Heading>DevJobs</Heading>
                     <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Description>
                     <ButtonRow>
-                        <LogInButton>Sign In</LogInButton>
-                        <SignUpButton>Sign Up</SignUpButton>
+                        <ButtonOutline>Sign In</ButtonOutline>
+                        <ButtonPrimary>Sign Up</ButtonPrimary>
                     </ButtonRow>
                     <p tw="my-8 text-gray-500">You need to have an account to register/post jobs</p>
                 </HeaderContent>
+                <JobsNav>
+                    <h2 tw="font-bold text-2xl mb-4">Posted Jobs</h2>
+                    <ul tw="inline-flex items-center text-sm md:text-base font-bold">
+                        <li tw="cursor-pointer px-4 py-1 mx-2 bg-green-100 text-green-600">New Jobs</li>
+                        <li>|</li>
+                        <li tw="cursor-pointer px-4 py-1 mx-2">1 Week Ago</li>
+                        <li>|</li>
+                        <li tw="cursor-pointer px-4 py-1 mx-2">2 Weeks Ago</li>
+                        <li>|</li>
+                        <li tw="cursor-pointer px-4 py-1 mx-2">1 Month Ago</li>
+                    </ul>
+                </JobsNav>
             </Header>
 
-            {loading ? (
-                <p>Loading please wait</p>
-            ) : (
-                <>
-                    {jobs.length && (
-                        jobs?.map(job => {
-                            return (
-                                <div
-                                    className="p-4 my-4 shadow-lg bg-gray-100"
-                                    key={job.pk}
-                                >
-                                    <p key="1">pk: {job.pk}</p>
-                                    <p key="2">Company ID: {job.company_id}</p>
-                                    <p key="3">Title: {job.title}</p>
-                                    <p key="4">Company Name: {job.company_name}</p>
-                                    <p key="5">Duration: {job.duration}</p>
-                                    <p key="6">experience_level": {job.experience_level}</p>
-                                    <p key="7">expected_salary": {job.expected_salary}</p>
-                                    <p key="8">Users Applied": {job.users_applied}</p>
-                                    <p key="9">Created Date": {job.created_date}</p>
-                                    <a key="10" href={job.url}>learn more about this job</a>
-                                </div>
-                            )
-                        }))}
-                </>
-            )}
+            <hr tw="mx-12" />
 
-            <Button onClick={() => handleRefresh()}>refresh jobs</Button>
+            <div tw="p-4 md:p-12 flex flex-col md:flex-row">
+                {loading ? (
+                    <p>Loading please wait</p>
+                ) : (
+                    <>
+                        {jobs.length && (
+                            jobs?.map(job => {
+                                return (
+                                    <div className="p-8 my-4 rounded-lg shadow-lg md:w-1/3 text-gray-500 border hover:border-green-500" key={job.pk} >
+                                        <div tw="mb-4">
+                                            <h3 tw="font-bold text-3xl mb-4 text-gray-700">{job.title}</h3>
+                                            <p>Company : {job.company_name}</p>
+                                            <p>Company Name: {job.company_name}</p>
+                                            <p>Duration: {job.duration}</p>
+                                            <p>experience_level": {job.experience_level}</p>
+                                            <p>expected_salary": {job.expected_salary}</p>
+                                            <p>Users Applied": {job.users_applied}</p>
+                                            <p>Created Date": {job.created_date}</p>
+                                        </div>
+                                        <button tw="w-full p-2 rounded font-bold bg-green-500 hocus:bg-green-600 text-white mb-2">Apply</button>
+                                        <a a href={job.url} tw="block text-center w-full p-2 rounded font-bold text-green-500 border border-green-500 hocus:bg-green-600 hocus:text-white">See Full Details</a>
+                                    </div>
+                                )
+                            }))}
+                    </>
+                )}
+            </div>
 
+            <p tw="text-center text-3xl text-green-600 font-bold cursor-pointer my-12" onClick={() => handleRefresh()}>See More Jobs</p>
+            <hr tw="mx-12" />
+
+            <HeaderContent tw="my-12">
+                <h4 tw="text-gray-500 text-3xl font-bold">To see And Post More Job Offers</h4>
+                <ButtonRow>
+                    <ButtonOutline>Sign In</ButtonOutline>
+                    <ButtonPrimary>Sign Up</ButtonPrimary>
+                </ButtonRow>
+                <p tw="my-8 text-gray-500">You need to have an account to register/post jobs</p>
+            </HeaderContent>
         </Container>
-
     )
 }
 
