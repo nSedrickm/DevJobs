9047896b7a74942978ab8a034ea8a8b9e452cc0f
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-21 11:38:52 +0100

    chore: install dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import "styles/output.css";
 import LandingPage from 'pages/LandingPage';
 
 function App() {
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import tw from "twin.macro";
 import { getJobs } from "services/api.service";
 import "pages/style.css";
+
+const Button = tw.button`p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600`;
+
 const LandingPage = () => {
 
     const [jobs, setJobs] = useState({})
@@ -61,8 +65,7 @@ const LandingPage = () => {
     return (
         <div className="App">
             <h1>Fetched Jobs</h1>
-            <button className="button"
-                onClick={() => handleRefresh()}>refresh jobs</button>
+            <Button onClick={() => handleRefresh()}>refresh jobs</Button>
 
             {loading ? (
                 <p>Loading please wait</p>
@@ -90,6 +93,19 @@ const LandingPage = () => {
                         }))}
                 </>
             )}
+
+
+            <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
+                <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
+                <p className="text-gray-400 mt-5 text-lg">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                    consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
+                    eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
+                </p>
+                <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+                    Hello Friends 🚀
+                </button>
+            </div>
         </div>
     )
 }
