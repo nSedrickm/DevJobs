25c73cbd12895f31a6c0f0dff173872c48a08d89
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-23 20:50:02 +0100

    update: add job seeker about me page

--- src/pages/JobSeekerRegistrationPage.js
+++ src/pages/JobSeekerRegistrationPage.js
@@ -7,6 +7,7 @@ import { GrStackOverflow } from "react-icons/gr";
 
 const Input = tw.input`border border-gray-600 w-full mt-2 mb-4 p-2 px-4 placeholder-gray-400 text-sm rounded bg-opacity-90 hocus:outline-none focus:ring-green-600 focus:border-green-600`;
 const Select = tw.select`border border-gray-600 w-full mt-2 mb-4 p-2 px-4 placeholder-gray-400 text-sm rounded bg-opacity-90 hocus:outline-none focus:ring-green-600 focus:border-green-600`;
+const TextArea = tw.textarea`w-full rounded-lg mb-6 hocus:outline-none focus:ring-green-600 focus:border-green-600`;
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -43,35 +44,42 @@ const JobSeekerRegistrationPage = () => {
                             className={state.stage === 3 && "text-green-600"}
                             tw="cursor-pointer px-4 py-1 mx-2"
                         >
-                            Personal Information
+                            Personal
                         </li>
                         <li><BsArrowRight size={24} /></li>
                         <li onClick={() => dispatch({ type: "changeStage", payload: 4 })}
                             className={state.stage === 4 && "text-green-600"}
                             tw="cursor-pointer px-4 py-1 mx-2"
                         >
-                            Contact Information
+                            Contact
                         </li>
                         <li><BsArrowRight size={24} /></li>
                         <li onClick={() => dispatch({ type: "changeStage", payload: 5 })}
                             className={state.stage === 5 && "text-green-600"}
                             tw="cursor-pointer px-4 py-1 mx-2"
                         >
-                            Stack/Role Information
+                            Stack / Role
                         </li>
                         <li><BsArrowRight size={24} /></li>
                         <li onClick={() => dispatch({ type: "changeStage", payload: 6 })}
                             className={state.stage === 6 && "text-green-600"}
                             tw="cursor-pointer px-4 py-1 mx-2"
                         >
-                            About Me
+                            Location
                         </li>
                         <li><BsArrowRight size={24} /></li>
                         <li onClick={() => dispatch({ type: "changeStage", payload: 7 })}
                             className={state.stage === 7 && "text-green-600"}
                             tw="cursor-pointer px-4 py-1 mx-2"
                         >
-                            Cv/Resume
+                            About Me
+                        </li>
+                        <li><BsArrowRight size={24} /></li>
+                        <li onClick={() => dispatch({ type: "changeStage", payload: 8 })}
+                            className={state.stage === 8 && "text-green-600"}
+                            tw="cursor-pointer px-4 py-1 mx-2"
+                        >
+                            Cv / Resume
                         </li>
                     </ul>
                 </header>
@@ -89,7 +97,7 @@ const JobSeekerRegistrationPage = () => {
 
                         <div tw="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 mx-auto bg-white md:shadow-lg md:rounded-xl">
 
-                            <form tw="p-8">
+                            <form tw="p-4 sm:p-8">
                                 <label tw="block">First Name</label>
                                 <Input type="text" placeholder="John Doe" />
 
@@ -126,7 +134,7 @@ const JobSeekerRegistrationPage = () => {
 
                         <div tw="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 mx-auto bg-white md:shadow-lg md:rounded-xl">
 
-                            <form tw="p-8">
+                            <form tw="p-4 sm:p-8">
                                 <label tw="block">Email</label>
                                 <Input type="email" placeholder="email@example.com" />
 
@@ -150,20 +158,69 @@ const JobSeekerRegistrationPage = () => {
                     <>
                         <header tw="w-full flex justify-center items-center my-4">
                             <GrStackOverflow size={24} tw="mr-4 text-green-600" />
-                            <h1 tw="text-2xl  font-bold ">Stack/Role Information</h1>
+                            <h1 tw="text-xl sm:text-2xl  font-bold ">Stack/Role Information</h1>
                         </header>
 
-                        <div tw="w-full sm:w-2/3 md: w-1/2 lg:w-1/3 mx-auto bg-white md:shadow-lg md:rounded-xl">
+                        <div tw="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 mx-auto bg-white md:shadow-lg md:rounded-xl">
 
-                            <form tw="p-8">
+                            <form tw="p-4 sm:p-8">
                                 <label tw="block">Stack, Dev Role</label>
-                                <Input type="text" placeholder="LAMP" />
+                                <Input type="text" placeholder="Front End" />
 
                                 <label tw="block">Experience Level</label>
                                 <Input type="text" placeholder="5 years" />
 
                                 <label tw="block">Salary/Pay Range</label>
-                                <Input type="text" placeholder="username" />
+                                <Input type="text" placeholder="$150000" />
+
+
+                                <button onClick={() => dispatch({ type: "changeStage", payload: 6 })} tw="w-full p-2 bg-green-600 text-center font-bold text-white rounded-md mt-2">Next</button>
+                            </form>
+                        </div>
+                    </>
+                )}
+
+                {state.stage === 6 && (
+                    <>
+                        <header tw="w-full flex justify-center items-center my-4">
+                            <GrStackOverflow size={24} tw="mr-4 text-green-600" />
+                            <h1 tw="text-xl sm:text-2xl  font-bold ">Location Information</h1>
+                        </header>
+
+                        <div tw="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 mx-auto bg-white md:shadow-lg md:rounded-xl">
+
+                            <form tw="p-4 sm:p-8">
+                                <label tw="block">Country</label>
+                                <Input type="text" placeholder="Nigeria" />
+
+                                <label tw="block">State</label>
+                                <Input type="text" placeholder="Lagos" />
+
+                                <label tw="block">City</label>
+                                <Input type="text" placeholder="Lagos" />
+
+
+                                <button onClick={() => dispatch({ type: "changeStage", payload: 7 })} tw="w-full p-2 bg-green-600 text-center font-bold text-white rounded-md mt-2">Next</button>
+                            </form>
+                        </div>
+                    </>
+                )}
+
+                {state.stage === 7 && (
+                    <>
+                        <header tw="w-full flex justify-center items-center my-4">
+                            <BsPeopleCircle size={24} tw="mr-4 text-green-600" />
+                            <h1 tw="text-xl sm:text-2xl  font-bold ">About Me</h1>
+                        </header>
+
+                        <div tw="w-full sm:w-2/3 md:w-1/2  mx-auto bg-white md:shadow-lg md:rounded-xl">
+
+                            <form tw="p-4 sm:p-8">
+                                <label tw="block text-center mb-2">About Me</label>
+                                <TextArea rows="8"></TextArea>
+
+
+
 
                                 <button onClick={() => dispatch({ type: "changeStage", payload: 6 })} tw="w-full p-2 bg-green-600 text-center font-bold text-white rounded-md mt-2">Next</button>
                             </form>
