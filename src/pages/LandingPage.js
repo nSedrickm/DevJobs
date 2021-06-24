6a0cffb4ed57cb12eb28488dfc42586013f4706e
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-24 16:37:06 +0100

    update: landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,23 +5,27 @@ import heroBg from "images/hero.svg";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { getJobs } from "services/api.service";
+import { FiArrowRightCircle } from 'react-icons/fi';
 
-const Container = tw.div`w-full text-gray-800`;
+const Container = tw.div`w-full text-gray-800 bg-white`;
 const Header = styled.header`
-  ${tw`pt-24 md:py-32 text-center md:h-screen md:relative`}
+  ${tw`pt-24 md:py-32 text-center md:h-screen md:relative bg-white`}
   background-image: url(${heroBg});
   background-size: contain;
   background-position: top center;
   background-repeat: no-repeat;
 `;
 const HeaderContent = tw.div`grid place-items-center p-8`;
-const JobsNav = tw.div`lg:absolute bottom-20 inset-x-12 flex flex-col md:flex-row justify-between`;
 const Heading = tw.h1`text-green-600 text-6xl md:text-8xl font-bold`;
 const Description = tw.p`text-lg my-8 tracking-wide text-gray-700`;
 const ButtonRow = tw.div`mt-4 flex flex-col md:flex-row md:justify-between`;
 const Button = tw(Link)`px-24 py-3 mx-4 rounded-lg font-bold text-white mt-5 hover:bg-green-700`;
 const ButtonPrimary = tw(Button)`bg-green-600`;
 const ButtonOutline = tw(Button)`border border-green-600 text-green-600 hover:bg-green-100`;
+const JobContainer = tw.div`p-8 lg:p-20 flex flex-col md:flex-row`;
+const JobsNav = tw.div`p-8 lg:px-20 text-center md:text-left flex flex-col items-center justify-center lg:flex-row lg:justify-between`;
+const JobCard = tw.div`p-5 my-4 rounded-lg shadow-lg md:w-1/2 lg:w-1/3 text-gray-500 border hover:border-green-600 hover:shadow-none`;
+const Divider = tw.hr`mx-20 border-gray-900`;
 
 const LandingPage = () => {
 
@@ -91,23 +95,25 @@ const LandingPage = () => {
                     </ButtonRow>
                     <p tw="my-8 text-gray-500">You need to have an account to register/post jobs</p>
                 </HeaderContent>
-                <JobsNav>
-                    <h2 tw="font-bold text-2xl mb-4">Posted Jobs</h2>
-                    <ul tw="inline-flex items-center text-sm md:text-base font-bold">
-                        <li tw="cursor-pointer px-4 py-1 mx-2 bg-green-100 text-green-700">New Jobs</li>
-                        <li>|</li>
-                        <li tw="cursor-pointer px-4 py-1 mx-2">1 Week Ago</li>
-                        <li>|</li>
-                        <li tw="cursor-pointer px-4 py-1 mx-2">2 Weeks Ago</li>
-                        <li>|</li>
-                        <li tw="cursor-pointer px-4 py-1 mx-2">1 Month Ago</li>
-                    </ul>
-                </JobsNav>
             </Header>
 
-            <hr tw="mx-12" />
 
-            <div tw="p-4 md:p-12 flex flex-col md:flex-row">
+            <JobsNav>
+                <h2 tw="font-bold text-2xl mb-4 lg:mb-0">Posted Jobs</h2>
+                <ul tw="inline-flex items-center text-sm md:text-base font-bold">
+                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2 bg-green-100 text-green-700">New Jobs</li>
+                    <li>|</li>
+                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2">1 Week Ago</li>
+                    <li>|</li>
+                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2">2 Weeks Ago</li>
+                    <li>|</li>
+                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2">1 Month Ago</li>
+                </ul>
+            </JobsNav>
+
+            <Divider />
+
+            <JobContainer>
                 {loading ? (
                     <p>Loading please wait</p>
                 ) : (
@@ -115,7 +121,7 @@ const LandingPage = () => {
                         {jobs.length && (
                             jobs?.map(job => {
                                 return (
-                                    <div className="p-8 my-4 rounded-xl shadow-md md:w-1/3 text-gray-500 border hover:border-green-600 hover:shadow-none" key={job.pk} >
+                                    <JobCard key={job.pk} >
                                         <div tw="mb-4">
                                             <h3 tw="font-bold text-3xl mb-4 text-gray-700">{job.title}</h3>
                                             <p>Company : {job.company_name}</p>
@@ -128,18 +134,19 @@ const LandingPage = () => {
                                         </div>
                                         <button tw="w-full p-2 rounded font-bold bg-green-600 hocus:bg-green-700 text-white mb-2">Apply</button>
                                         <a a href={job.url} tw="block text-center w-full p-2 rounded font-bold text-green-600 border border-green-600 hocus:bg-green-100">See Full Details</a>
-                                    </div>
+                                    </JobCard>
                                 )
                             }))}
                     </>
                 )}
-            </div>
+            </JobContainer>
 
-            <p tw="text-center text-3xl text-green-700 font-bold cursor-pointer my-12" onClick={() => handleRefresh()}>See More Jobs</p>
-            <hr tw="mx-12" />
+            <p tw="text-center text-3xl text-green-700 font-bold cursor-pointer my-12 flex items-center justify-center" onClick={() => handleRefresh()}>See More Jobs &nbsp; <FiArrowRightCircle /></p>
 
-            <HeaderContent tw="my-12 text-center">
-                <h4 tw="text-gray-500 text-3xl font-bold">To see And Post More Job Offers</h4>
+            <Divider />
+
+            <HeaderContent tw="py-24 text-center">
+                <h4 tw="text-gray-500 text-2xl font-bold">To see And Post More Job Offers</h4>
                 <ButtonRow>
                     <ButtonOutline to="/login">Sign In</ButtonOutline>
                     <ButtonPrimary to="/signup">Sign Up</ButtonPrimary>
