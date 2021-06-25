034565d668529a4e731e292f83aef1814caadbd8
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-25 14:24:33 +0100

    udpate : landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,13 +6,12 @@ import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { getJobs } from "services/api.service";
 import { FiArrowRightCircle } from 'react-icons/fi';
+import { Loader } from 'components';
 
 const Container = tw.div`w-full text-gray-800 bg-white`;
 const Header = styled.header`
-  ${tw`pt-24 md:py-32 text-center md:h-screen md:relative bg-white`}
+  ${tw`pt-24 md:pt-32 text-center md:h-screen md:relative bg-white bg-cover md:bg-contain md:bg-center`}
   background-image: url(${heroBg});
-  background-size: contain;
-  background-position: top center;
   background-repeat: no-repeat;
 `;
 const HeaderContent = tw.div`grid place-items-center p-8`;
@@ -22,10 +21,20 @@ const ButtonRow = tw.div`mt-4 flex flex-col md:flex-row md:justify-between`;
 const Button = tw(Link)`px-24 py-3 mx-4 rounded-lg font-bold text-white mt-5 hover:bg-green-700`;
 const ButtonPrimary = tw(Button)`bg-green-600`;
 const ButtonOutline = tw(Button)`border border-green-600 text-green-600 hover:bg-green-100`;
-const JobContainer = tw.div`p-8 lg:p-20 flex flex-col md:flex-row`;
-const JobsNav = tw.div`p-8 lg:px-20 text-center md:text-left flex flex-col items-center justify-center lg:flex-row lg:justify-between`;
-const JobCard = tw.div`p-5 my-4 rounded-lg shadow-lg md:w-1/2 lg:w-1/3 text-gray-500 border hover:border-green-600 hover:shadow-none`;
-const Divider = tw.hr`mx-20 border-gray-900`;
+const InlineLoader = tw(props => <Loader {...props} />)``;
+
+const JobContainer = tw.div`p-4 sm:p-8 lg:p-20 flex flex-col md:flex-row justify-center`;
+const JobNav = tw.nav`px-4 sm:px-8 pt-20 pb-10 lg:px-20 text-center md:text-left flex flex-col items-center justify-center lg:flex-row lg:justify-between`;
+const JobNavTitle = tw.h2`font-bold text-3xl mb-8 lg:mb-0`;
+const JobNavUl = tw.ul`inline-flex items-center`;
+const JobNavLi = tw.li`font-semibold text-sm sm:text-base cursor-pointer p-2 py-1 `;
+const JobCard = tw.div`p-5 m-4 rounded-lg shadow-lg md:w-1/2 lg:w-1/3 bg-white text-gray-500 border hover:border-green-600 hover:shadow-none`;
+const JobCardTitle = tw.h3`font-bold text-xl md:text-2xl mb-4 text-gray-700`;
+const JobCardBody = tw.div`mb-4`;
+const JobMeta = tw.div`flex flex-row md:inline-flex text-sm lg:text-base py-2`;
+const ApplyButton = tw.button`block w-full p-1 sm:p-2 rounded font-bold bg-green-600 hocus:bg-green-700 text-white mb-3`;
+const DetailsButton = tw.a`block text-center w-full p-1 sm:p-2 rounded font-bold text-green-600 border border-green-600 hocus:bg-green-100`;
+const Divider = tw.hr`mx-20 border-gray-300`;
 
 const LandingPage = () => {
 
@@ -37,7 +46,6 @@ const LandingPage = () => {
         getJobs()
             .then(response => {
                 setJobs(response.data);
-                console.log(response.data);
                 setLoading(false);
             })
             .catch(error => {
@@ -98,42 +106,50 @@ const LandingPage = () => {
             </Header>
 
 
-            <JobsNav>
-                <h2 tw="font-bold text-2xl mb-4 lg:mb-0">Posted Jobs</h2>
-                <ul tw="inline-flex items-center text-sm md:text-base font-bold">
-                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2 bg-green-100 text-green-700">New Jobs</li>
-                    <li>|</li>
-                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2">1 Week Ago</li>
-                    <li>|</li>
-                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2">2 Weeks Ago</li>
-                    <li>|</li>
-                    <li tw="cursor-pointer px-2 sm:px-4 py-1 mx-2">1 Month Ago</li>
-                </ul>
-            </JobsNav>
+            <JobNav>
+                <JobNavTitle>Posted Jobs</JobNavTitle>
+                <JobNavUl>
+                    <JobNavLi tw=" bg-green-100 text-green-700">New Jobs</JobNavLi>
+                    <JobNavLi>|</JobNavLi>
+                    <JobNavLi >1 Week Ago</JobNavLi>
+                    <JobNavLi>|</JobNavLi>
+                    <JobNavLi >2 Weeks Ago</JobNavLi>
+                    <JobNavLi>|</JobNavLi>
+                    <JobNavLi >1 Month Ago</JobNavLi>
+                </JobNavUl>
+            </JobNav>
 
             <Divider />
 
             <JobContainer>
                 {loading ? (
-                    <p>Loading please wait</p>
+                    <InlineLoader tw="h-96" />
                 ) : (
                     <>
                         {jobs.length && (
                             jobs?.map(job => {
                                 return (
                                     <JobCard key={job.pk} >
-                                        <div tw="mb-4">
-                                            <h3 tw="font-bold text-3xl mb-4 text-gray-700">{job.title}</h3>
-                                            <p>Company : {job.company_name}</p>
-                                            <p>Company Name: {job.company_name}</p>
-                                            <p>Duration: {job.duration}</p>
-                                            <p>experience_level": {job.experience_level}</p>
-                                            <p>expected_salary": {job.expected_salary}</p>
-                                            <p>Users Applied": {job.users_applied}</p>
-                                            <p>Created Date": {job.created_date}</p>
-                                        </div>
-                                        <button tw="w-full p-2 rounded font-bold bg-green-600 hocus:bg-green-700 text-white mb-2">Apply</button>
-                                        <a a href={job.url} tw="block text-center w-full p-2 rounded font-bold text-green-600 border border-green-600 hocus:bg-green-100">See Full Details</a>
+                                        <JobCardBody>
+                                            <JobCardTitle>{job.title}</JobCardTitle>
+                                            <JobMeta>
+                                                <p>Company : {job.company_name}</p>
+                                            </JobMeta>
+                                            <JobMeta>
+                                                <p>Lagos,Nigeria</p>
+                                                <p tw="mx-1 md:mx-2">|</p>
+                                                <p>fulltime</p>
+                                                <p tw="mx-1 md:mx-2">|</p>
+                                                <p>$20000000/year</p>
+                                            </JobMeta>
+                                            <JobMeta>
+                                                <p tw="text-center">{job.users_applied ? job.users_applied : 0} applies</p>
+                                                <p tw="mx-1 md:mx-2">|</p>
+                                                <p>Posted {new Date(job.created_date).toLocaleString()}</p>
+                                            </JobMeta>
+                                        </JobCardBody>
+                                        <ApplyButton>Apply</ApplyButton>
+                                        <DetailsButton href={job.url}>See Full Details</DetailsButton>
                                     </JobCard>
                                 )
                             }))}
@@ -141,7 +157,8 @@ const LandingPage = () => {
                 )}
             </JobContainer>
 
-            <p tw="text-center text-3xl text-green-700 font-bold cursor-pointer my-12 flex items-center justify-center" onClick={() => handleRefresh()}>See More Jobs &nbsp; <FiArrowRightCircle /></p>
+            <p tw="text-center text-3xl text-green-700 font-bold cursor-pointer py-12 flex items-center justify-center" onClick={() => handleRefresh()}>See More Jobs &nbsp; <FiArrowRightCircle /></p>
+
 
             <Divider />
 
