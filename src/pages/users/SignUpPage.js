c27845f847c5b23abc2e8308f4cd5c5d89233f12
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-06-25 08:28:12 +0100

    feat: users can now register

--- src/pages/users/SignUpPage.js
+++ src/pages/users/SignUpPage.js
@@ -1,43 +1,29 @@
-import React, { useReducer } from "react";
+import React, { useState } from "react";
 import tw from "twin.macro";
+import toast from "react-hot-toast";
+import Loader from "components/Loader";
+
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { registerUser } from "services/auth.service";
 
 const Input = tw.input`border border-green-600 w-full mt-2 mb-2 p-2 px-4 placeholder-gray-400 text-sm rounded bg-opacity-90 hocus:outline-none focus:ring-green-600 focus:border-green-600`;
 const ErrorMessage = tw.p`text-sm text-red-500 mb-2`;
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'changeStage':
-            return {
-                ...state,
-                stage: action.payload
-            };
-        case 'loading':
-            return {
-                ...state,
-                loading: action.payload
-            };
-        default:
-            throw new Error();
-    }
-}
 
 const SignupSchema = yup.object().shape({
     username: yup.string().required('UserName is required'),
     password1: yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
     password2: yup.string().min(8, 'Password must be at least 8 characters').required('Confirm Password is required')
-    .oneOf([yup.ref('password1'), null], 'Password does not match'),
+        .oneOf([yup.ref('password1'), null], 'Password does not match'),
 });
 
 
 const SignUpPage = () => {
 
-    const [state, dispatch] = useReducer(reducer, {
-        stage: 1,
-        loading: false
-    });
+    const [stage, setStage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     const {
         register,
@@ -46,12 +32,46 @@ const SignUpPage = () => {
     } = useForm({
         resolver: yupResolver(SignupSchema)
     });
-    const onSubmit = (data) => {
-        alert(JSON.stringify(data));
-        dispatch({ type: "changeStage", payload: 2 });
-    };
 
-    if (state.stage === 2) {
+    const handleSignUp = (data) => {
+
+        setLoading(true);
+
+        registerUser(data)
+            .then(response => {
+                toast.success("Registration Successfull");
+                console.log(response.data)
+                setLoading(false);
+                setStage(2);
+            })
+            .catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    if (error.response.status === 400) {
+                        if (error.response.data.username) {
+                            toast.error(error.response.data.username[0]);
+                        }
+                        else if(error.response.data.password1) {
+                            toast.error(error.response.data.password1[0]);
+                        }
+                    }
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    toast.error("An error occurred Please check your network and try again");
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    toast.error("An error occurred Please check your network and try again");
+                }
+                setLoading(false);
+            });
+    }
+
+    if (loading) return <Loader />
+
+    if (stage === 2) {
         return (
             <section tw="text-gray-600 md:p-24 h-screen">
                 <div tw="mx-auto p-12 lg:mx-36 bg-white md:shadow-lg md:rounded-xl">
@@ -84,7 +104,7 @@ const SignUpPage = () => {
                         <p tw="text-green-600 my-4 text-sm">Terms & Privacy | cookie policy</p>
                     </div>
                     <div tw="w-full md:w-1/2 p-4">
-                        <form tw="w-5/6 md:w-2/3 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+                        <form tw="w-5/6 md:w-2/3 mx-auto" onSubmit={handleSubmit(handleSignUp)}>
                             <label tw="block text-sm mt-2">User Name</label>
                             <Input
                                 type="text"
