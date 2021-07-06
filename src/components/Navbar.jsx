3a919a16467b0096776f7c086b0b626afa076a8d
tolulopzy <christabelodeyemi@gmail.com>
2021-07-06 12:54:16 +0100

    update: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { Link } from "react-router-dom";
 import { useUserContext } from "pages/UserContext";
 import { Popover, Menu, Position } from "evergreen-ui";
 
-const MainHeader = tw.div`bg-white`;
-const Brand = tw.img`font-bold text-4xl text-green-600 ml-2 p-2 mr-auto`;
+const MainHeader = tw.div`bg-white z-50 sticky inset-x-0 top-0 shadow-lg`;
+const Brand = tw.img`font-bold text-4xl text-primary ml-2 p-2 mr-auto`;
 const Nav = tw.nav`inline-flex`;
 const NavLink = tw(Link)`inline-flex px-4 py-2 hocus:text-green-700`;
-const Input = tw.input`border border-green-600 w-96 my-2 p-1.5 px-8 rounded-md bg-opacity-90 hocus:outline-none focus:ring-green-600 focus:border-green-600`;
+const Input = tw.input`border border-primary w-96 my-2 p-1.5 px-8 rounded-md bg-opacity-90 hocus:outline-none focus:ring-primary focus:border-primary`;
 const Button = tw(Link)`inline-flex px-4 py-2 mx-2 rounded-md font-bold hover:bg-green-700`;
-const ButtonSignUp = tw(Button)`bg-green-600 text-white`;
-const ButtonLogIn = tw(Button)`border border-green-600 text-green-600  hover:bg-green-100`;
-const ButtonLogOut = tw.button`inline-flex items-center justify-center px-4 py-2 mx-2 rounded-md font-bold hover:bg-green-700 bg-green-600 text-white`;
+const ButtonSignUp = tw(Button)`bg-primary text-white`;
+const ButtonLogIn = tw(Button)`border border-primary text-primary  hover:bg-green-100`;
+const ButtonLogOut = tw.button`inline-flex items-center justify-center px-4 py-2 mx-2 rounded-md font-bold hover:bg-green-700 bg-primary text-white`;
 const SearchBar = tw.div`relative mx-auto`;
 const SearchIcon = tw(FiSearch)`absolute left-2 inset-y-5`;
 const DesktopNav = tw.div`hidden lg:flex items-center justify-between h-16 px-4`;
@@ -38,8 +38,8 @@ const Navbar = () => {
                     <SearchIcon />
                 </SearchBar>
                 <Nav>
-                    <NavLink to="/" tw="text-green-600">Home</NavLink>
-                    <NavLink to="/dashboard">Employer (Post Jobs)</NavLink>
+                    <NavLink to="/" tw="text-primary">Home</NavLink>
+                    {/* <NavLink to="/dashboard">Employer (Post Jobs)</NavLink> */}
                     {!state.key ? (
                         <>
                             <ButtonSignUp to="/signup">Sign Up</ButtonSignUp>
@@ -53,11 +53,11 @@ const Navbar = () => {
                                     <Menu>
                                         <Menu.Divider />
                                         <Link to="/users/profile">
-                                        <Menu.Item
-                                            icon={FiUser}
-                                        >
-                                            Profile
-                                        </Menu.Item>
+                                            <Menu.Item
+                                                icon={FiUser}
+                                            >
+                                                Profile
+                                            </Menu.Item>
                                         </Link>
                                         <Menu.Item
                                             icon={FiLogOut}
