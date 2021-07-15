9c45cfdf2ab7ef11638d94dd7dc012c5b9400cf0
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-07-15 13:27:03 +0100

    feat: create empty state component

---
+++ src/components/EmptyState.js
@@ -0,0 +1,33 @@
+import React from "react";
+import tw, { styled } from "twin.macro";
+import PropTypes from "prop-types";
+import illustration from "images/empty_street.svg";
+
+const Container = styled.div`
+  ${tw`grid h-56 text-center bg-center bg-contain place-items-center`}
+  background-image: linear-gradient(rgba(255,255,255,0.68), rgba(255,255,255,0.68)),url(${illustration});
+  background-repeat: no-repeat;
+`;
+const Content = tw.div`flex flex-col items-center`;
+const Text = tw.p`text-black text-2xl font-bold`;
+
+const EmptyState = (props) => {
+    return (
+        <Container>
+            <Content>
+                <Text>No Available Items</Text>
+
+                <button onClick={props.onClick}
+                    className="block px-8 py-2 mx-auto my-4 text-center text-white rounded-md text-bold bg-primary hover:-translate-y-8 hover:shadow-lg">
+                    refresh
+                </button>
+            </Content>
+        </Container>
+    );
+}
+
+EmptyState.propTypes = {
+    onClick: PropTypes.func
+}
+
+export default EmptyState;
